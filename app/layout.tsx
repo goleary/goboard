71ce227ae80190ef6c9980aa16731d46f10d7b67
Gabe O'Leary <oleary.gabe@gmail.com>
2024-03-30 16:20:01 -0700

    add umami

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import classNames from "classnames";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,6 +13,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://umami-production-9fe7.up.railway.app/script.js"
+          data-website-id="3bb05bc8-9b50-4ed9-9a15-313979906ae2"
+        ></Script>
+      </head>
       <body className={classNames(inter.className, "bg-slate-100")}>
         {children}
       </body>
