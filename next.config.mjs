79eaeaf5e793ac7db07810161b7be53e4c0c49d2
Gabe O'Leary <oleary.gabe@gmail.com>
2024-03-30 19:00:19 -0700

    revert

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/lake-stats",
-  assetPrefix: "/lake-stats",
+  // basePath: "/lake-stats",
+  // assetPrefix: "/lake-stats",
   // redirects: async () => {
   //   return [
   //     {
