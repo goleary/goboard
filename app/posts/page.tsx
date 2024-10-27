0a1801e6aedc5f24e4555307ce4ed03356d0af60
Gabe O'Leary <oleary.gabe@gmail.com>
2024-10-27 15:56:41 -0700

    optimize mobile styling

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -18,10 +18,10 @@ const Posts: React.FC = () => {
               href={`/posts/${post.id}`}
               className="grid grid-cols-5 group gap-4"
             >
-              <sub className="text-sm text-gray-500 text-right">
+              <sub className="text-sm text-gray-500 text-right col-span-2 md:col-span-1">
                 {dayjs(post.date).format("MMMM D, YYYY")}
               </sub>
-              <h1 className="text-lg font-bold col-span-4 group-hover:underline text-slate-900">
+              <h1 className="text-lg font-bold col-span-3 md:col-span-4 group-hover:underline text-slate-900">
                 {post.title}
               </h1>
             </Link>
